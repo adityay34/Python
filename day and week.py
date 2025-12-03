da4d54935b46220b062c388fcbedd15a1aa122b2
Aditya Yadav <adityayadu2233@gmail.com>
2025-12-03 21:19:05 +0530

    Days and Weeks

---
+++ day and week.py
@@ -0,0 +1,20 @@
+a=int(input("Enter a number(1-7):"))
+if a==1:
+    print("Sunday")
+elif a==2:
+    print("Monday")
+else:
+    if a==3:
+        print("Tuesday")
+    elif a==4:
+        print("Wednesday")
+    else:
+        if a==5:
+            print("Thrusday")
+        elif a==6:
+            print("Friday")
+        else:
+            if a==7:
+                print("Saturday")
+            else:
+                print("invalid number")
